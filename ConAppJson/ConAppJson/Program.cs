0e643c3fb75d1443ff565a3e6cc286334c37794c
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-07-05 21:20:42 +0800

    check in share code

---
+++ ConAppJson/ConAppJson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ConAppJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "";
+            SSISDBEntities context = new SSISDBEntities();
+
+            var result = from r in context.Customers where (r.CustomerID == "ALFKI") select r;
+
+            foreach(Customer customer in result)
+            {
+                json = new JavaScriptSerializer().Serialize(customer);
+                json = "[" + json + "]";
+                Console.WriteLine(json);
+            }            
+        }
+    }
+}
